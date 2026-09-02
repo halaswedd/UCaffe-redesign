10b8e1b2c781d95bb8027bd49cc2bddded88d8b7
Celine Sleem <celinesleemchs@gmail.com>
2026-09-02 21:16:15 +0300

    db

--- backend/config/db.php
+++ backend/config/db.php
@@ -2,22 +2,15 @@
 
 $host = getenv("DB_HOST");
 $port = getenv("DB_PORT");
-$username = getenv("DB_USER");
+$user = getenv("DB_USER");
 $password = getenv("DB_PASSWORD");
 $database = getenv("DB_NAME");
 
-$conn = new mysqli(
-    $host,
-    $username,
-    $password,
-    $database,
-    $port
-);
+$conn = new mysqli($host, $user, $password, $database, $port);
 
 if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
 }
 
 $conn->set_charset("utf8mb4");
-
 ?>
